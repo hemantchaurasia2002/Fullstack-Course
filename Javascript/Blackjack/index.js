ebf4975f8d658db92bb75b490dc7a0e67e577371
hemantchaurasia2002 <chaurasiahemant2002@gmail.com>
2026-08-15 23:35:56 +0530

    commit on 15-08-2026

---
+++ Javascript/Blackjack/index.js
@@ -0,0 +1,32 @@
+let firstCard = 10
+let secondCard = 4 
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+let messageEl = document.getElementById("message-el")
+console.log(messageEl)
+let sumEl = document.getElementById("sum-el")
+console.log(sumEl)
+let cardsEl = document.getElementById("cards-el")
+console.log(cardsEl)
+
+function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack!"
+    hasBlackJack = true
+    } else {
+    message = "You're out of the game!"
+    isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+}
--- Javascript/booleans.js
+++ Javascript/booleans.js
@@ -3,9 +3,11 @@ let hasDiscountCode = true
 function processOrder() {
     if (hasDiscountCode) {
         console.log('Discount applied to food order')
+        hasDiscountCode = false
     } else {
-        console.log('No Discount applied to food order')
+        console.log('No Discount applied')
     }
 }
 
-processOrder ()
+processOrder()
+processOrder()

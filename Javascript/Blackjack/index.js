036af00c2be6a8eb44604c5c6b8ee790fb52e0f3
hemantchaurasia2002 <chaurasiahemant2002@gmail.com>
2026-09-07 22:53:38 +0530

    commit on 07 September 2026

--- Javascript/Blackjack/index.js
+++ Javascript/Blackjack/index.js
@@ -1,5 +1,6 @@
 let firstCard = 10
-let secondCard = 4 
+let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -13,7 +14,15 @@ let cardsEl = document.getElementById("cards-el")
 console.log(cardsEl)
 
 function startGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i=0; i<cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
     message = "Do you want to draw a new card?"
@@ -28,5 +37,9 @@ function startGame() {
 }
 
 function newCard() {
-    console.log("Drawing a new card from the deck!")
+    let card = getRamdomCard()
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    renderGame()
 }
